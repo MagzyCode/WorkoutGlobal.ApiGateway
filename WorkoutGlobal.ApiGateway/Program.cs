eedb145f50b0a28ea85362ea6b807d4b3f2183dd
MagzyCode <mihailkazarevich@mail.ru>
2022-09-09 11:56:52 +0300

    Create base configuration of routes.

--- WorkoutGlobal.ApiGateway/Program.cs
+++ WorkoutGlobal.ApiGateway/Program.cs
@@ -1,6 +1,19 @@
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile(
+    path: builder.Configuration["OcelotConfiguration"]);
+
+var ocelotConfiguration = new ConfigurationBuilder()
+                            .AddJsonFile(builder.Configuration["OcelotConfiguration"])
+                            .Build();
+
+builder.Services.AddOcelot(ocelotConfiguration)
+    .AddCacheManager(settings => settings.WithDictionaryHandle());
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
 app.Run();
+app.UseOcelot();
